bfb244e0ee5d5d350b73954e5596e1c27c0e0e06
MargaretBarseghayn <69532991+MargaretBarseghayn@users.noreply.github.com>
2020-11-06 16:46:31 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte humanAge = 14;
+        System.out.println("humanAge = " + humanAge);
+        short number = 56;
+        System.out.println("number = " + number);
+        int intNumber = 4575;
+        System.out.println("intNumber = " + intNumber);
+        long longNumber = 12L;
+        System.out.println("longNumber = " + longNumber);
+        float floatNum = 0.12F;
+        System.out.println("floatNum = " + floatNum);
+        double doubleNumber = 12.1D;
+        System.out.println("doubleNumber = " + doubleNumber);
+        boolean isTrue = false;
+        System.out.println("isTrue = " + isTrue);
+    }
+}
+
+
+
